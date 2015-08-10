861e848d9aa36a0f03687a2377d5b7a6a67696e6
Lazose <shes92147@gmail.com>
2015-08-10 10:11:55 +0800

    ver3.1

---
+++ jave104/src/Javatest26.java
@@ -0,0 +1,27 @@
+ import java . util.Scanner;
+public class Javatest26 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scn = new  Scanner(System.in);
+		System.out.println();
+        
+		int n = scn.nextInt();
+		
+		
+		for( int i= 0 ;  i < n ; i++ ){
+		for( int j= 0 ;  j <= i ; j++ ){
+			
+			System.out.print("*");	
+		}
+		System.out.println();
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
