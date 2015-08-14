4b23c0e5fd95344f114595fbb7d37307298f9785
Lazose <shes92147@gmail.com>
2015-08-14 12:04:16 +0800

    ver5.1

--- javatest/src/Javatest39.java
+++ javatest/src/Javatest39.java
@@ -1,9 +1,21 @@
-
+import java.util.Scanner;
 public class Javatest39 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("請輸入一段數字");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
