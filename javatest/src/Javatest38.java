3953c801a04683d1cb0c58ed358fa31c51931393
Lazose <shes92147@gmail.com>
2015-08-14 12:01:10 +0800

    ver5.0

--- javatest/src/Javatest38.java
+++ javatest/src/Javatest38.java
@@ -5,7 +5,44 @@ public class Javatest38 {
 		// TODO Auto-generated method stub
 
 		Scanner scn = new Scanner(System.in);
-		System.out.println("");
+		System.out.println("請輸入A矩陣");
+		int x = scn. nextInt();
+		int y = scn. nextInt();
+		System.out.println("請輸入B矩陣");
+		int z = scn. nextInt();
+		int s = scn. nextInt();
+		
+		
+		int p = 0 ;
+		int q = 0 ;		
+		
+		if (y != z){
+		System.out.println("無此矩陣!!");
+		//  例如:  3*4 4*5 = 3*5
+		
+		}
+		
+		int data1[][] = new int[x][y];
+		int data2[][] = new int[z][s];
+		int data3[][] = new int[x][s];
+		
+		System.out.println("輸入A矩陣數字");
+		
+		for (int u = 0 ; u < x ; u++ ){
+		for (int m = 0 ; m < y ; m++ ){
+		     data1[u][m] = scn . nextInt();
+		
+		}
+		}
+		
+		System.out.println("輸入B矩陣數字");
+		
+		for (int m = 0 ; m < z ; m++){
+		for (int n = 0 ; n < s ; n++){
+		     data2[m][n] = scn . nextInt();
+		
+		}
+		}
 		
 		
 		
@@ -14,21 +51,20 @@ public class Javatest38 {
 		
 		
 		
-		
-		
-		
-		
-		
-		
-		int p = 0 ; int q = 0 ;
-				for(int i = 0; i<2 ; i++){
-		            for(int j = 0; j<2 ; j++){
-		                for(int k = 0; k<3 ; k++ ){
-		                    c[p][q]=data1[i][k]*data2[k][j];
+
+				for(int u = 0; u<x ; u++){
+		            for(int m = 0; m<y ; m++){
+		                for(int n = 0; n<s ; n++ ){
+		                    data3[p][q]=data1[u][m]*data2[m][n];
 		
 		                }q++;
 		                }p++;q=0;
 		                }
-
+				for (p = 0; p < x; p++) {
+					for (q = 0; q < s; q++) {
+						System.out.print(data3[p][q] + "\t");
+					                        }
+					System.out.println();
+                                        }
 }
 }
