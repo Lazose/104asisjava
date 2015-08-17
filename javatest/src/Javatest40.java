7a6a0bf3c9a0d7774ced87a90f2a0d44a96d6c86
Lazose <shes92147@gmail.com>
2015-08-17 16:10:35 +0800

    ver5.3

--- javatest/src/Javatest40.java
+++ javatest/src/Javatest40.java
@@ -1,8 +1,27 @@
-
+import java.util.Scanner;
+import java.util.Random;
 public class Javatest40 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		Scanner scn = new Scanner(System.in);
+		Random rnd = new Random();
+		int n =1000- rnd.nextInt(1000);
+		System.out.println("找"+n+"元");
+		int v1=n/500;
+		 n=n%500;
+		int v2=n/100;
+		 n=n%100;
+		int v3=n/50;
+         n=n%50;
+		int v4=n/10;
+         n=n%10;
+        int v5=n/5;
+         n=n%5;
+        int v6=n/1; 
+         n=n%1;
+         System.out.println(v1+"個五百"+v2+"個一百"+v3+"個五十"+v4+"個十"+v5+"個五"+v6+"個一");
 
 	}
 
---
+++ javatest/src/Javatest41.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Javatest41 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		Random rnd = new Random();
+		int n1[]=[500.100.50.10.5.1];
+		int n2[]=new int [6];
+		for (int i = 0;i<n1.length;i++){
+		int n2[i]=n/n1[i];
+		int n=n%n1[i];
+		if(n2[i]>0){
+		System.out.println(n2[i]+"­Ó"+n1[i]);
+		
+		}
+		}
+		
+	}
+
+}
