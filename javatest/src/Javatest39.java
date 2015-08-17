cbae1417c9c3bf7e77896a23b88781fe769b0b34
Lazose <shes92147@gmail.com>
2015-08-17 13:43:35 +0800

    ver5.2

--- javatest/src/Javatest39.java
+++ javatest/src/Javatest39.java
@@ -3,19 +3,24 @@ public class Javatest39 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("請輸入一段數字");
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+        Scanner scn = new Scanner(System.in); 
+		System.out.println("n");
+		int n = scn.nextInt();
+		System.out.println(n+"!="+fac(n));
+	}
+	public static int fac(int v1){	
+	if(v1==1){	
+		return 1 ;
+	}else{
+		return v1*fac(v1-1);
+	}
+	}	
+	public static long fun1(int v1){
+		long result = 1;
+		for (int i = 1 ; i <=v1; i++){
+		result*=i;
+	}
+	return result;
 	}
 
 }
