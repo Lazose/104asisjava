2332758971a2b020705bc63cc1562f9f1b0c0884
Lazose <shes92147@gmail.com>
2015-08-25 14:13:13 +0800

    ver10.3

---
+++ javatest/src/Work11.java
@@ -0,0 +1,90 @@
+import java . awt.*;
+import java . awt . event.*;
+import javax . swing.*; 
+import java.util.Scanner;
+import java.util.Random;
+public class Work11 extends JFrame{
+
+	private JLabel name = new JLabel();
+    private JTextField jlb1 = new JTextField();
+    private JButton back = new JButton("返回");
+    private JLabel talk = new JLabel();
+    private Container cp ;
+    private String fin   ;
+	//------------------------------------------------
+	public  Work11(){
+	initComp();
+}
+	private void initComp(){	
+	cp = this . getContentPane();
+	this.setBounds(150,100,600,400);	
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);	
+	cp.setLayout(null);//視窗
+	//--------------------------------------
+	
+	name.setBounds(75,60,250,250);	//設定 標籤-標示輸贏
+	name.setText(fin);
+	cp.add(name);
+	System.out.println(name.getText());
+	
+	//--------------------------------------
+	back.setBounds(50,70,250,30);//push 按鍵-返回
+	cp.add(back);
+	back.addActionListener(new ActionListener(){
+	public void actionPerformed(ActionEvent ae){
+	
+	
+	}	
+	});}
+	//--------------------------------------
+	//剪刀石頭布 判斷
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner scn = new Scanner(System.in);
+	Random rnd = new Random();	
+	
+	
+	
+	int w =0; int l =0;int t =0;//贏或輸或平手
+	for( int z = 0 ; z<=2 ; z++){
+	
+	//-------------------------------------------
+	
+	int y ;int x = 1 ;//雙方出的
+	int[]data= new int[1];
+	data[y]=rnd.nextInt(3);
+	int vi = '1-y';
+    
+	switch(vi){
+	
+    	
+
+	case (1):
+	
+	z++;l++;
+	break;	
+	case (2):
+		
+	z++;w++;
+	break;
+	case (-1):
+	 
+	z++;w++;
+	break;
+	case (-2):	
+	
+	z++;l++;
+	break;	
+	
+	default :
+	
+	z++;t++;
+	break;
+	
+	} 
+	}
+
+
+}
+}
--- javatest/src/Work4.java
+++ javatest/src/Work4.java
@@ -6,10 +6,12 @@ import java. util.Random;
 	
 public class Work4 extends JFrame{
 
-	private JLabel name = new JLabel("請輸入姓名");
+	
+	private JLabel jlb2 = new JLabel();
     private JTextField jlb1 = new JTextField("名字");
     private JButton sure = new JButton("確定");
     private JLabel talk = new JLabel();
+    private String name = " 請輸入名字" ;
     private Container cp ;
     //----------------------------------------------
     //設定視窗
@@ -21,6 +23,11 @@ public class Work4 extends JFrame{
     	this.setBounds(150,100,600,400);	
     	this.setDefaultCloseOperation(EXIT_ON_CLOSE);	
     	cp.setLayout(null);
+    	
+    	//-----------------------------------------	
+    	jlb2.setBounds(75,60,250,250);	//設定 標籤
+    	jlb2.setText(name);
+    	cp.add(jlb1);
     	
     	//-----------------------------------------
     	jlb1.setBounds(50,70,250,25);	//設定 -名字輸入
