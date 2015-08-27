a54b884b8ff680f30d39962fa907b71260bfcf25
Lazose <shes92147@gmail.com>
2015-08-27 09:57:57 +0800

    ver10.8

--- javatest/src/insertpicture.java
+++ javatest/src/insertpicture.java
@@ -4,37 +4,47 @@ import javax . swing.*;
 import java.util.Scanner;
 import java.util.Random;
 public class insertpicture {
+	
+	
+	
 	public static void main(String[] args) {
 	
 		// TODO Auto-generated method stub
+	
+	//---------------------------標示圖片在電腦位置
+	ImageIcon image0 = new ImageIcon("file://E:GitHub/pa .jpg");
+	ImageIcon image1 = new ImageIcon("file://E:GitHub/se .jpg");
+	ImageIcon image2 = new ImageIcon("file://E:Github/st .jpg");
+		
 	//-------------------------標示空間label
 	
-	private JLabel JLb1  = new JLabel();
-	private JLabel JLb2  = new JLabel();
-	private String image0;
-	private String image1;
+	JLabel JLb1  = new JLabel();
+	JLabel JLb2  = new JLabel();
+	JLabel JLb3  = new JLabel();
+
+	
+	//--------------------------加入圖片
+	JLb1.add(JLb1);
+	JLb2.add(JLb2);
+	JLb2.add(JLb3);
+
 	//--------------------------標示label位置
+	
 	JLb1.setIcon(image0);
 	JLb1.setBounds(0,0,800,600);
 	JLb2.setIcon(image1);
 	JLb2.setBounds(0,0,800,600);
+	JLb3.setIcon(image2);
+	JLb3.setBounds(0,0,800,600);
 	
 	
-	//--------------------------加入圖片
-	JLb1.add(img0);
-	JLb2.add(img1);
-	
-	//---------------------------標示圖片在電腦位置
-	ImageIcon img1 = new ImageIcon("file://E: .jpg");
-	ImageIcon img2 = new ImageIcon("file://E: .jpg");
+
+
 	
 	
 	
 	
 	
-	
-	
-	
-	
+	}
 	
 }
